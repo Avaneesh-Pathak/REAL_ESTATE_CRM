c79c1da72df21856314273917b62ded16e7b90fd
Avaneesh Pathak <124344318+Avaneesh-Pathak@users.noreply.github.com>
2024-10-05 22:42:56 +0530

    First Commit

---
+++ leads/tests.py
@@ -0,0 +1,9 @@
+# from django.test import TestCase
+
+# # Create your tests here.
+# class LandingPageTest(TestCase):
+#     def test_status_code(self):
+
+
+#     def test_template_name(self):
+
---
+++ leads/views.py
@@ -0,0 +1,187 @@
+from typing import Any
+from django.core.mail import send_mail
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect,reverse
+from django.http import HttpResponse
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm,CustomUserCreationForm
+from django.views import generic 
+from agents.mixins import OrganisorAndLoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+#CRUD - Create, Retrieve, Update and Delete + List
+
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")    
+
+
+
+class LandingPageView(generic.TemplateView):
+    template_name = "landing.html"
+
+
+def landing_page(request):
+    return render(request,"landing.html")
+    
+class LeadListView(LoginRequiredMixin,generic.ListView):
+    template_name = "leads/lead_list.html"
+    context_object_name = "leads"
+
+    def get_queryset(self):
+        user = self.request.user
+        # initial queryset of leads for the entire organisation
+        if user.is_organisor:
+            queryset = Lead.objects.filter(
+                organisation=user.userprofile, 
+                agent__isnull=False
+            )
+        else:
+            queryset = Lead.objects.filter(
+                organisation=user.agent.organisation)
+            # filter for the agent that is logged in
+            queryset = queryset.filter(agent__user=user)
+        return queryset
+      
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads":leads
+    }
+    return render(request,"leads/lead_list.html",context)
+
+class LeadDetailView(LoginRequiredMixin,generic.DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
+
+def lead_detail(request,pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead" : lead
+
+    }
+    return render(request,"leads/lead_detail.html",context)
+
+
+
+class LeadCreateView(OrganisorAndLoginRequiredMixin,generic.CreateView):
+    template_name = "leads/lead_create.html"
+    form_class  = LeadModelForm
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+    
+    def form_valid(self, form):
+        # todo send email
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView,self).form_valid(form)
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == "POST":
+        print('Recieving')
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
+
+class LeadUpdateView(OrganisorAndLoginRequiredMixin,generic.UpdateView):
+    template_name = "leads/lead_update.html"
+    queryset=Lead.objects.all()
+    form_class  = LeadModelForm
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        #print('Recieving')
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,
+        "lead": lead
+    }
+    return render(request, "leads/lead_update.html", context)
+
+
+class LeadDeleteView(OrganisorAndLoginRequiredMixin,generic.DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset=Lead.objects.all()
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+
+# def lead_update(request,pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+#     if request.method == "POST":
+#         # print('Recieving')
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             agent = Agent.objects.first()
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+#             return redirect("/leads")
+#     context = {
+
+#         "form": form,
+#         "lead": lead
+#     }
+#     return render(request, "leads/lead_update.html", context)
+
+
+# def lead_create(request):
+    # form = LeadModelForm()
+    # if request.method == "POST":
+    #     print('Recieving')
+    #     form = LeadForm(request.POST)
+    #     if form.is_valid():
+    #         print(form.cleaned_data)
+    #         first_name = form.cleaned_data['first_name']
+    #         last_name = form.cleaned_data['last_name']
+    #         age = form.cleaned_data['age']
+    #         agent = Agent.objects.first()
+    #         Lead.objects.create(
+    #             first_name = first_name,
+    #             last_name=last_name,
+    #             age=age,
+    #             agent=agent
+    #         )
+    #         return redirect("/leads")
+
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "leads/lead_create.html", context)
