1c0301fa0f974e9a75e600f12bd232faae9d4b74
Avaneesh Pathak <124344318+Avaneesh-Pathak@users.noreply.github.com>
2024-10-06 21:21:26 +0530

    Uassigned Leads

--- CRM/urls.py
+++ CRM/urls.py
@@ -2,18 +2,31 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from django.contrib.auth.views import LoginView,LogoutView
-from leads.views import landing_page,LandingPageView,SignupView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView, 
+    PasswordResetCompleteView
+)
+from leads.views import LandingPageView, SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',LandingPageView.as_view(), name = 'landing_page'),
-    path('leads/', include('leads.urls', namespace="leads")), 
-    path('agents/', include('agents.urls', namespace="agents")), 
-    path('signup/', SignupView.as_view(), name="signup"), 
-    path('login/', LoginView.as_view(), name="login"), 
-    path('logout/', LogoutView.as_view(), name="logout"), 
-     
+    path('', LandingPageView.as_view(), name='landing_page'),
+    path('leads/', include('leads.urls', namespace='leads')),
+    path('agents/', include('agents.urls', namespace='agents')),
+    path('signup/', SignupView.as_view(), name='signup'),
+
+    # Password reset URLs
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('reset-password-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset-password-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset-password-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    lead_list,lead_detail,lead_create,lead_update, lead_delete, LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
+    lead_list,lead_detail,lead_create,lead_update, lead_delete, LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView,AssignAgentView
 )
 
 app_name = 'leads'  # Add this line
@@ -12,7 +12,7 @@ urlpatterns = [
     path('<int:pk>/delete/',LeadDeleteView.as_view(), name='lead-delete'),
     path('create/',LeadCreateView.as_view(),name='lead-create'),
     # path('json/', LeadJsonView.as_view(), name='lead-list-json'),
-    # path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
+    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
     # path('<int:pk>/category/', LeadCategoryUpdateView.as_view(), name='lead-category-update'),
     # path('<int:pk>/followups/create/', FollowUpCreateView.as_view(), name='lead-followup-create'),
     # path('followups/<int:pk>/', FollowUpUpdateView.as_view(), name='lead-followup-update'),
