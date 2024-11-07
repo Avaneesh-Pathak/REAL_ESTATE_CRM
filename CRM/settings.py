c6ada7663ee8d943702e4ed39ca529eef819d788
Avaneesh Pathak <124344318+Avaneesh-Pathak@users.noreply.github.com>
2024-11-07 12:08:59 +0530

    Last Commit

--- CRM/settings.py
+++ CRM/settings.py
@@ -138,14 +138,14 @@ LOGOUT_REDIRECT_URL = "/"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 # Email settings
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" if DEBUG else "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" if DEBUG else "django.core.mail.backends.smtp.EmailBackend"
 # Use an actual email backend for production, such as:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'avaneeshpathak900@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Anuj@03717P'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'avaneeshpathak900@gmail.com'
+EMAIL_HOST_PASSWORD = 'yvga yoxu bzse epbd'
 
 # # Secure settings for production
 # if not DEBUG:
